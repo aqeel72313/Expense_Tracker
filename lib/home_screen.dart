252528f1fb8178b467ef3f38646d0b12d51a1183
aqeel72313 <iftikharaqeel86@gmail.com>
2026-08-31 16:21:04 +0500

    Dashboard Container

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -44,7 +44,7 @@ class HomeScreen extends StatelessWidget{
                         "Rs.12,456",
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.surface,
-                      fontSize: 14,
+                      fontSize: 16,
                       ),
                     ),
                     SizedBox(height: 10),
@@ -52,14 +52,70 @@ class HomeScreen extends StatelessWidget{
                         "August 2026",
                     style: TextStyle(
                       color:Theme.of(context).colorScheme.surface,
-                    ),
+                      ),
                     )
+                  ],
+                ),
+              ),
+              SizedBox(height: 25),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    "Recent Expense",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                        Icons.restaurant_rounded,
+                      size: 30,
+                    ),
+                    SizedBox(width: 15),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Food",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Today",
+                        style: TextStyle(
+                          fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Text(
+                      "Rs. 850",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
               ),
             ],
           ),
         ),
-      );
+    );
   }
 }
