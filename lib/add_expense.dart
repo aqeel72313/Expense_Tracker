c17c84ecaec85ba83bd6b92a8e7a530704bdbfa7
aqeel72313 <iftikharaqeel86@gmail.com>
2026-09-04 23:16:18 +0500

    ...

--- lib/add_expense.dart
+++ lib/add_expense.dart
@@ -18,7 +18,41 @@ class AddExpenseScreen extends StatelessWidget {
       body: Padding(
           padding: EdgeInsets.all(20),
         child: Column(
-
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+                "Expense Title",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 6),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter Expense Title",
+                filled: true,
+                fillColor: Theme.of(context).colorScheme.surface,
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 16,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.secondary,
+                    width: 2,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
