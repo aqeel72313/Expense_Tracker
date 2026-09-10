f2b0999c4365ebcd59e0e1306cfbd77b3654350e
aqeel72313 <iftikharaqeel86@gmail.com>
2026-09-10 23:47:40 +0500

    ...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -191,7 +191,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                    "Recent Expenses",
+                    "Recent Expense",
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
