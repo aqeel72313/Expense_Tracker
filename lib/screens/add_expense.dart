c174fad5d0ae451fe7fa88e00673cae1f28d9f00
aqeel72313 <iftikharaqeel86@gmail.com>
2026-09-10 23:50:22 +0500

    ...

--- lib/screens/add_expense.dart
+++ lib/screens/add_expense.dart
@@ -293,7 +293,7 @@ DateTime? selectedDate;
                   print(selectedDate);
                 },
                 child: Text(
-                  "Sav",
+                  "Save",
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.surface,
                   fontSize: 22
