1dc927d51c364b2a52dc9788d779fd6a142b48ab
aqeel72313 <iftikharaqeel86@gmail.com>
2026-09-12 12:21:32 +0500

    ...

--- lib/screens/add_expense.dart
+++ lib/screens/add_expense.dart
@@ -38,6 +38,97 @@ DateTime? selectedDate;
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Category Field
+              Text(
+                "Category",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 6),
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            title: Text("Food"),
+                            onTap: () {
+                              setState(() {
+                                selectedCategory = "Food";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            title: Text("Transport"),
+                            onTap: () {
+                              setState(() {
+                                selectedCategory = "Transport";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            title: Text("Shopping"),
+                            onTap: () {
+                              setState(() {
+                                selectedCategory = "Shopping";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            title: Text("Bills"),
+                            onTap: () {
+                              setState(() {
+                                selectedCategory = "Bills";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            title: Text("Other"),
+                            onTap: () {
+                              setState(() {
+                                selectedCategory = "Other";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.secondary,
+                      width: 2,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        selectedCategory,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      Icon(Icons.keyboard_arrow_down_rounded),
+                    ],
+                  ),
+                ),
+              ),
+            SizedBox(height: 16),
 
             // Expense Title Field
             Text(
@@ -113,98 +204,6 @@ DateTime? selectedDate;
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 16),
-
-            // Category Field
-            Text(
-              "Category",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 6),
-            GestureDetector(
-              onTap: () {
-                showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        title: Text("Food"),
-                        onTap: () {
-                          setState(() {
-                            selectedCategory = "Food";
-                          });
-                          Navigator.pop(context);
-                        },
-                      ),
-                      ListTile(
-                        title: Text("Transport"),
-                        onTap: () {
-                          setState(() {
-                            selectedCategory = "Transport";
-                          });
-                          Navigator.pop(context);
-                        },
-                      ),
-                      ListTile(
-                        title: Text("Shopping"),
-                        onTap: () {
-                          setState(() {
-                            selectedCategory = "Shopping";
-                          });
-                          Navigator.pop(context);
-                        },
-                      ),
-                      ListTile(
-                        title: Text("Bills"),
-                        onTap: () {
-                          setState(() {
-                            selectedCategory = "Bills";
-                          });
-                          Navigator.pop(context);
-                        },
-                      ),
-                      ListTile(
-                        title: Text("Other"),
-                        onTap: () {
-                          setState(() {
-                            selectedCategory = "Other";
-                          });
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-              },
-            child: Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(
-                  color: Theme.of(context).colorScheme.secondary,
-                  width: 2,
-                ),
-              ),
-              child: Row(
-                children: [
-                  Text(
-                      selectedCategory,
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                  Icon(Icons.keyboard_arrow_down_rounded),
-                ],
-              ),
-            ),
             ),
             SizedBox(height: 16),
 
