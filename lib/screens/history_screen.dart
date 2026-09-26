cc07a82327d961d6c5c8ad625e01e79fb13427ea
aqeel72313 <iftikharaqeel86@gmail.com>
2026-09-26 23:45:24 +0500

    ...

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -208,6 +208,14 @@ class _HistoryScreenState extends State<HistoryScreen>{
                                       return AlertDialog(
                                         title: const Text("Delete Expense"),
                                         content: const Text("Are you sure you want to delete this expense?"),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: (){
+                                                Navigator.pop(context, false);
+                                              },
+                                              child: const Text("Cancel"),
+                                          ),
+                                        ],
                                       );
                                     },
                                 );
